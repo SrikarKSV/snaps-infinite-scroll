30dd63a6c8ab5100a00216beac2622feb0468e4c
Srikar KSV <srikar1awesome@gmail.com>
2021-04-07 19:14:16 +0530

    📦 NEW: Prototyped the query fetch

---
+++ src/js/Image.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+export default class Image {
+  static #page = 1;
+
+  constructor(data) {
+    this.id = data.id;
+    this.photographer = data.photographer;
+    this.avgColor = data.avg_color;
+    this.originalLink = data.src.original;
+    this.compressedLink = `${this.originalLink}?auto=compress`;
+    this.mediumLink = `${this.compressedLink}&cs=tinysrgb&h=350`;
+  }
+
+  giveHtmlNode(index) {
+    const html = /* html */ `
+    <div>
+      <img src="${this.mediumLink}" data-index="${index}" alt="Taken by ${this.photographer}"/>
+    </div>
+    `;
+    return document.createRange().createContextualFragment(html);
+  }
+
+  static async #fetchData(url) {
+    const { data } = await axios.get(url, {
+      headers: {
+        Authorization: process.env.API_KEY,
+      },
+    });
+    return data;
+  }
+
+  static async fetchImagesQuery(query) {
+    const data = await this.#fetchData(
+      `https://api.pexels.com/v1/search?query=${encodeURIComponent(
+        query
+      )}&page=${this.#page}`
+    );
+    this.#page++;
+    return data;
+  }
+  // TODO: Make a static method to fetch results based on query and id
+  // TODO: Make a static method to check if an id is bookmarked (in LS)
+  // TODO: Make a method to bookmark an image to storage and toggles property
+  // TODO: Make a method to unbookmark an image from storage and toggles property
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,26 @@
-console.log('it works!');
-console.log(process.env.API_KEY);
+import Image from './Image';
+
+const searchForm = document.querySelector('.main__form form');
+const imgGrid = document.querySelector('.img-grid');
+
+searchForm.addEventListener('submit', handleSearchSubmission);
+
+let images = [];
+
+async function handleSearchSubmission(e) {
+  e.preventDefault();
+  if (!e.target.image.value) return;
+  const query = e.target.image.value;
+
+  const queryResults = await Image.fetchImagesQuery(query);
+  const photos = queryResults.photos.map((photo) => new Image(photo));
+  const fragment = document.createDocumentFragment();
+  photos.forEach((photo, index) => {
+    fragment.append(photo.giveHtmlNode(index));
+  });
+
+  imgGrid.append(fragment);
+
+  images = [...images, ...photos];
+  console.log(images);
+}
