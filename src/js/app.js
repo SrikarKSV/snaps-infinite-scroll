a044ca3f949482a3301f1a9aa61dd693e5bee446
Srikar KSV <srikar1awesome@gmail.com>
2021-04-08 15:02:46 +0530

    📦 NEW: Prtotyped the intersection observer

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,62 @@ import Image from './Image';
 const searchForm = document.querySelector('.main__form form');
 const imgGrid = document.querySelector('.img-grid');
 const loadingAnimation = document.querySelector('.loading-animation');
+const intersectionObserver = document.querySelector('.intersection-observer');
 
 searchForm.addEventListener('submit', handleSearchSubmission);
+imgGrid.addEventListener('click', handleImageClick);
 
-let images = [];
+let allImages = [];
+let isNextAvailable = false;
+let query;
+let loading = false;
 
+// Handle search query
 async function handleSearchSubmission(e) {
   e.preventDefault();
   if (!e.target.image.value) return;
-  const query = e.target.image.value;
+  query = e.target.image.value;
 
   toggleLoadingAnimation('start');
   Image.resetPage(); // Resetting page number for new query
   const queryResults = await Image.fetchImagesQuery(query);
+  isNextAvailable = !!queryResults.next_page;
   toggleLoadingAnimation('end');
   const photos = appendImages(queryResults); // Results fed and photos instances are returned
 
-  images = [...photos];
-  console.log(images);
+  allImages = [...photos];
+  console.log(allImages);
+}
+
+function handleImageClick(e) {
+  const element = e.target;
+  if (element.matches('img')) {
+    const {
+      dataset: { index },
+    } = element;
+    console.log(allImages[index]);
+  }
+}
+
+// Handle infinte scroll
+const ob = new IntersectionObserver(handleInfiniteScroll, {
+  rootMargin: '50%',
+  threshold: [0, 0.5, 1],
+});
+ob.observe(intersectionObserver);
+
+async function handleInfiniteScroll(payload, observer) {
+  if (!isNextAvailable || loading) return;
+  // console.log(payload[0]);
+  if (payload[0].intersectionRatio > 0.5) {
+    console.log('YES!');
+    console.log(payload[0]);
+    const queryResults = await Image.fetchImagesQuery(query);
+    isNextAvailable = !!queryResults.next_page;
+    const photos = appendImages(queryResults); // Results fed and photos instances are returned
+
+    allImages = [...allImages, ...photos];
+  }
 }
 
 function appendImages(images) {
@@ -28,7 +66,7 @@ function appendImages(images) {
     const photos = images.photos.map((photo) => new Image(photo));
     const fragment = document.createDocumentFragment();
     photos.forEach((photo, index) => {
-      fragment.append(photo.giveHtmlNode(index));
+      fragment.append(photo.giveHtmlNode(index + allImages.length));
     });
     imgGrid.append(fragment);
     return photos;
@@ -36,10 +74,16 @@ function appendImages(images) {
     const h4 = document.createElement('h4');
     h4.innerText = 'No results found';
     imgGrid.append(h4);
+    return [];
   }
 }
 
 function toggleLoadingAnimation(state) {
-  state === 'start' ? (imgGrid.innerHTML = '') : null;
+  if (state === 'start') {
+    imgGrid.innerHTML = '';
+    loading = true;
+  } else {
+    loading = false;
+  }
   loadingAnimation.classList.toggle('show');
 }
