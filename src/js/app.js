372277b09e52885f77cc3dd763296ad1c5bf78e7
Srikar KSV <srikar1awesome@gmail.com>
2021-04-08 11:25:44 +0530

    👌 IMPROVE: Refactored fetching results and animation

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import Image from './Image';
 
 const searchForm = document.querySelector('.main__form form');
 const imgGrid = document.querySelector('.img-grid');
+const loadingAnimation = document.querySelector('.loading-animation');
 
 searchForm.addEventListener('submit', handleSearchSubmission);
 
@@ -12,15 +13,33 @@ async function handleSearchSubmission(e) {
   if (!e.target.image.value) return;
   const query = e.target.image.value;
 
+  toggleLoadingAnimation('start');
+  Image.resetPage(); // Resetting page number for new query
   const queryResults = await Image.fetchImagesQuery(query);
-  const photos = queryResults.photos.map((photo) => new Image(photo));
-  const fragment = document.createDocumentFragment();
-  photos.forEach((photo, index) => {
-    fragment.append(photo.giveHtmlNode(index));
-  });
+  toggleLoadingAnimation('end');
+  const photos = appendImages(queryResults); // Results fed and photos instances are returned
 
-  imgGrid.append(fragment);
-
-  images = [...images, ...photos];
+  images = [...photos];
   console.log(images);
+}
+
+function appendImages(images) {
+  if (images.photos.length) {
+    const photos = images.photos.map((photo) => new Image(photo));
+    const fragment = document.createDocumentFragment();
+    photos.forEach((photo, index) => {
+      fragment.append(photo.giveHtmlNode(index));
+    });
+    imgGrid.append(fragment);
+    return photos;
+  } else {
+    const h4 = document.createElement('h4');
+    h4.innerText = 'No results found';
+    imgGrid.append(h4);
+  }
+}
+
+function toggleLoadingAnimation(state) {
+  state === 'start' ? (imgGrid.innerHTML = '') : null;
+  loadingAnimation.classList.toggle('show');
 }
