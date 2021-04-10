175db26b752ad2ce4a7c5decd19ddb2a7dd0040a
Srikar KSV <srikar1awesome@gmail.com>
2021-04-10 10:34:39 +0530

    👌 IMPROVE: Added avg color as background

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,5 @@
+import { isDark } from './lib/utils';
+
 const modal = document.querySelector('.modal');
 const modalImage = document.querySelector('.modal__image img');
 const modalPhototgrapher = document.querySelector('.modal__photographer');
@@ -16,10 +18,16 @@ function handleCloseModal(e) {
 }
 
 export function fillModal(object, toggleModal = false) {
+  // No need to fetch the modal image as already loaded for gallery!
   modalImage.src = object.mediumLink;
   modalImage.alt = `Taken by ${object.photographer}`;
   modalPhototgrapher.innerText = object.photographer;
   modalColor.innerText = object.avgColor;
+
+  // Showing the Avg. color as background
+  modalColor.style.backgroundColor = object.avgColor;
+  // Setting text color based on background
+  modalColor.style.color = isDark(object.avgColor) ? '#000' : '#fff';
   modalOriginalLink.href = object.originalLink;
   modalCompressedLink.href = object.compressedLink;
   modalMediumLink.href = object.mediumLink;
